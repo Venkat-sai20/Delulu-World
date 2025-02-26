b35e6524c8cbff344f90d543097c7a614a6e32cf
Your name <name@example.com>
2025-02-26 10:44:57 -0500

    made changes already

--- Delulu/Program.cs
+++ Delulu/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
 using System.Diagnostics;
 
 Console.WriteLine("Hello, World!");
+new IComponentChangeService
